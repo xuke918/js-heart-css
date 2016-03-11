e971d70d34ed38f68a09456bcd208ba8c87871bf
xuke918 <xuke3070@gmail.com>
2016-03-11 16:14:00 -0500

    Added more $dl; dt; ul.

--- js/main.js
+++ js/main.js
@@ -10,8 +10,6 @@ var $liLastChild = $('li');
 var $dl = ('dl');
 var $dt = ('dt');
 
-
-
 $header.addClass('masthead');
 $h1.removeClass('big-label');
 $h1.addClass('logo');
@@ -20,3 +18,12 @@ $main.addClass('wrapper');
 
 $pFirstChild.addClass('intro');
 $h2.addClass('slug-head');
+
+$ul.addClass('slug-list');
+
+$img.addClass('slug-img');
+
+$dl.addClass('classification');
+$dt.removeClass('big-label');
+
+$liLastChild.show(liLastChild);
